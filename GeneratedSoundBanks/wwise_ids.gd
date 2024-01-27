056d9ff00b7ffc60357cff27e59294ab513c8f15
loweryjoshua <jbl101@utulsa.edu>
2024-01-27 12:53:06 -0600

    Wwise Integration

---
+++ GeneratedSoundBanks/wwise_ids.gd
@@ -0,0 +1,83 @@
+class_name AK
+
+class EVENTS:
+
+	const LUTEHERO_FLUBS = 1571823810
+	const LUTEHERO_MUSIC = 1320126269
+
+	const _dict = {
+		"LuteHero_Flubs": LUTEHERO_FLUBS,
+		"LuteHero_Music": LUTEHERO_MUSIC
+	}
+
+class STATES:
+
+	class MELODY:
+		const GROUP = 1040548709
+
+		class STATE:
+			const NONE = 748895195
+			const MELODYOFF = 875054468
+			const MELODYON = 2796181470
+
+	const _dict = {
+		"Melody": {
+			"GROUP": 1040548709,
+			"STATE": {
+				"None": 748895195,
+				"MelodyOFF": 875054468,
+				"MelodyON": 2796181470
+			}
+		}
+	}
+
+class SWITCHES:
+
+	const _dict = {}
+
+class GAME_PARAMETERS:
+
+	const _dict = {}
+
+class TRIGGERS:
+
+	const _dict = {}
+
+class BANKS:
+
+	const INIT = 1355168291
+
+	const _dict = {
+		"Init": INIT
+	}
+
+class BUSSES:
+
+	const FLUBS = 865863285
+	const MELODYLINES = 4115515182
+	const MASTER_AUDIO_BUS = 3803692087
+
+	const _dict = {
+		"Flubs": FLUBS,
+		"MelodyLines": MELODYLINES,
+		"Master Audio Bus": MASTER_AUDIO_BUS
+	}
+
+class AUX_BUSSES:
+
+	const _dict = {}
+
+class AUDIO_DEVICES:
+
+	const SYSTEM = 3859886410
+	const NO_OUTPUT = 2317455096
+
+	const _dict = {
+		"System": SYSTEM,
+		"No_Output": NO_OUTPUT
+	}
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {}
+
