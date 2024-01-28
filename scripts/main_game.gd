0cd8bd67fdec2149c49c7523140836e49bfb6512
Jeremy Robinson <jeremy@robros.design>
2024-01-28 13:46:31 -0600

    final

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var main_menu = $MainMenu
 @onready var lute_game = $with_lute
 @onready var juggle = $juggle
-
+const CREDITS = preload("res://credits.tscn")
 
 func _ready():
 	main_menu.game_started.connect(start_game)
@@ -12,3 +12,15 @@ func _ready():
 func start_game():
 	InputManager.stop_title.emit()
 	lute_game.start()
+
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_credits_pressed():
+	$hud/Control/MarginContainer/PanelContainer/MarginContainer/TextureRect.show()
+	%KingLabel.hide()
+	%PercentLabel.hide()
+	%BestLabel.hide()
